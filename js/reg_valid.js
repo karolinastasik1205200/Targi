0e8b0a7561a0382864be563129a3faa83e5b761d
ArielZ <arielzielinski2@gmail.com>
2024-05-06 20:55:34 +0200

    Style form and javascript validation in-progress

---
+++ js/reg_valid.js
@@ -0,0 +1,57 @@
+
+function validateForm() {
+    var username = document.forms["register-form"]["username"].value;
+    var errorUser = document.getElementById("jsValidUser");
+
+    var email = document.forms["register-form"]["email"].value;
+    var errorEmail = document.getElementById("jsValidEmail");
+
+    var password = document.forms["register-form"]["password"].value;
+    var errorPass = document.getElementById("jsValidPass");
+
+    var errors = ["", "", ""]; // Indeksy odpowiadają identyfikatorom elementów
+
+    if (!isUserNameValid(username)) {
+        errors[0] = "Dozwolone są tylko litery i cyfry oraz spacje w nazwie użytkownika!";
+    } else if (username.trim() === '') {
+        errors[0] = "Wpisz nazwę użytkownika!"
+    }
+
+    if (!validateEmail(email)) {
+        errors[1] = "Niewłaściwy format adresu e-mail!";
+    }
+
+    if (validatePass(password) < 6) {
+        errors[2] = "Hasło musi posiadać co najmniej 6 znaków!";
+    }
+
+    // Wyświetlenie błędów w odpowiednich miejscach na stronie
+    if (errors.some(error => error !== "")) {
+        errorUser.innerHTML = errors[0];
+        errorEmail.innerHTML = errors[1];
+        errorPass.innerHTML = errors[2];
+        return false;
+    } else {
+        errorUser.innerHTML = "";
+        errorEmail.innerHTML = "";
+        errorPass.innerHTML = "";
+        return true;
+    }
+}
+
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function isUserNameValid(username) {
+    var res = /^[a-zA-Z0-9][a-zA-Z0-9\s]*[a-zA-Z0-9]$/.exec(username);
+    var valid = !!res;
+    return valid;
+}
+
+function validatePass(password) {
+    var pass = String(password).length;
+    return pass;
+}
--- register_page.php
+++ register_page.php
@@ -32,30 +32,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 */
 ?>
-
+<script src="js/reg_valid.js"></script>
 <section class="section-register-form">
     <h1>Formularz rejestracji</h1>
-    <form action="register.php" method="post" class="register-form">
+    <form id="register-form" action="register.php" method="post" class="register-form">
         <div>
             <label for="username">Podaj nazwę użytkownika:</label>
             <input type="text" name="username" id="username" placeholder="Nazwa użytkownika" required>
+            <span id="jsValidUser"></span>
         </div>
 
         <div>
             <label for="email">Wprowadź swój adres e-mail:</label>
             <input type="email" name="email" id="email" placeholder="Adres e-mail" required>
+            <span id="jsValidEmail"></span>
         </div>
 
         <div>
             <label for="password">Podaj hasło:</label>
             <input type="password" name="password" id="password" placeholder="Hasło" required>
+            <span id="jsValidPass"></span>
 
             <label for="confirm-password">Potwierdź swoje hasło:</label>
             <input type="password" name="confirm_password" id="confirm-password" placeholder="Potwierdź hasło" required>
+            <span id="jsValidPass"></span>
         </div>
 
         <div>
-            <input type="submit" name="submit" value="Zarejestruj" class="register-btn">
+            <input type="submit" name="submit" value="Zarejestruj" class="register-btn" onclick="validateForm()">
         </div>
     </form>
 </section>

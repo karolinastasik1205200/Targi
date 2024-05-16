654863e83be1489844b7d2a19c58f20aaeb9c2aa
ArielZielinski <159643808+ArielZielinski@users.noreply.github.com>
2024-05-16 18:25:34 +0200

    Update home.php - Ariel preparations

--- home.php
+++ home.php
@@ -1,7 +1,10 @@
+<section class="siteContent">
+  
+</section>
 <?php
 echo "<h1> Tu powstanie strona Event-Arena. Zapraszamy niebawem </h1>";
 
-require "db_connect.php";
+// require "db_connect.php";
 
 
 ?>

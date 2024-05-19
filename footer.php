116db6315c3623240bf92adadcd2e2ec348d7069
maaniek697 <130924725+maaniek697@users.noreply.github.com>
2024-05-19 19:49:06 +0200

    foooter

--- footer.php
+++ footer.php
@@ -1,4 +1,16 @@
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9734.488430480846!2d16.891758422458945!3d52.413524702532094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470444c71a0ee0ff%3A0x691bb537edc8cad4!2zSmXFvHljZSwgUG96bmHFhCwgUG9sc2th!5e0!3m2!1spl!2snl!4v1712579399421!5m2!1spl!2snl" width="600" height="450" class="responsive-map" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<footer class="footer">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9734.488430480846!2d16.891758422458945!3d52.413524702532094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470444c71a0ee0ff%3A0x691bb537edc8cad4!2zSmXFvHljZSwgUG96bmHFhCwgUG9sc2th!5e0!3m2!1spl!2snl!4v1712579399421!5m2!1spl!2snl" width="200" height="200" class="responsive-map" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+   <span> 
+    <p>
+    <a href="https://www.facebook.com/your-page-name/" target="_blank">Odwiedź naszego facebooka</a>
+    </p>
+    <p>
+    <a href="https://www.instagram.com/your-username/" target="_blank">Zapraszamy na nasz profil na instagramie</a>
+    </p>
+    <p>
+    <a href=""></a>
+    </p>
+</span>
+    </footer>
 </body>
 </html>

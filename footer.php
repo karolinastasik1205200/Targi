473cf89b9526fe035f7de95c1ccce9f94bfa245d
maaniek697 <130924725+maaniek697@users.noreply.github.com>
2024-05-22 21:23:03 +0200

    w

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
 <footer class="footer">
-    <div>
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9734.488430480846!2d16.891758422458945!3d52.413524702532094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470444c71a0ee0ff%3A0x691bb537edc8cad4!2zSmXFvHljZSwgUG96bmHFhCwgUG9sc2th!5e0!3m2!1spl!2snl!4v1712579399421!5m2!1spl!2snl" width="100%" height="250" class="responsive-map" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</div>
-   <div class="div-size"> 
+  <span>
+    <div class="div-size"> 
     <h5>Social Media</h5>
     <p>
     <a href="#" target="_blank">Odwiedź naszego facebooka</a>
@@ -38,6 +37,7 @@
         <a href="#">Kontakt</a>
     </p>
 </div>
+</span>
     </footer>
 </body>
 </html>

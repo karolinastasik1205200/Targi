002ab751871530dcf246187ef7c66195e559c6f5
maaniek697 <130924725+maaniek697@users.noreply.github.com>
2024-05-31 13:56:29 +0200

    zmiany

--- home.php
+++ home.php
@@ -5,6 +5,21 @@
 echo "<h1> Tu powstanie strona Event-Arena. Zapraszamy niebawem </h1>";
 
 // require "db_connect.php";
-
+<div class="calendar-container">
+  <h2 class="section-title">Kalendarz wydarzeń</h2>
+  <div class="calendar">
+    <!-- kod kalendarza -->
+  </div>
+</div>
+<div class="upcoming-events-section">
+<h2>Nadchodzące wydarzenia</h2>
+  <!-- kalendarz z nadchodzącymi wydarzeniami -->
+</div>
+<div class="about-section">
+  <h2>O nas</h2>
+  <p>
+  Jesteśmy wiodącą firmą z Poznania, specjalizującą się w organizacji targów i wydarzeń branżowych. Nasza misja to tworzenie niezapomnianych doświadczeń, które łączą ludzi, pomysły i technologie. Dzięki wieloletniemu doświadczeniu oraz zaangażowaniu, zapewniamy najwyższy standard usług, dbając o każdy detal – od koncepcji po realizację. Nasze targi to platforma do nawiązywania wartościowych relacji biznesowych i wymiany wiedzy. Zapraszamy do współpracy i wspólnego tworzenia wyjątkowych wydarzeń, które inspirują i napędzają rozwój.
+  </p>
+</div>
 
 ?>

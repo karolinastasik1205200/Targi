ba1032d9ac162cf58433d7117a49e313b83faa15
ArielZ <arielzielinski2@gmail.com>
2024-06-02 13:20:38 +0200

    Poprawki

--- footer.php
+++ footer.php
@@ -1,34 +1,43 @@
-<footer class="footer" id="background">
-    <link rel="stylesheet" href="style.css">
-    <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9734.488430480846!2d16.891758422458945!3d52.413524702532094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470444c71a0ee0ff%3A0x691bb537edc8cad4!2zSmXFvHljZSwgUG96bmHFhCwgUG9sc2th!5e0!3m2!1spl!2snl!4v1712579399421!5m2!1spl!2snl"
-        width="100%" height="250" class="responsive-map" style="border:0;" allowfullscreen="" loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <footer>
-        <div class="footer-columns">
-            <div class="column">
-                <h4>Social Media</h4>
-                <ul id="tekst">
-                    <li><a href="#">Odwiedź naszego facebooka</a></li>
-                    <li><a href="#">Zapraszamy na nasz profil na instagramie</a></li>
-                    <li><a href="#">Wejdź na naszego youtuba</a></li>
-                </ul>
+    <footer class="footer">
+            <div>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9734.488430480846!2d16.891758422458945!3d52.413524702532094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470444c71a0ee0ff%3A0x691bb537edc8cad4!2zSmXFvHljZSwgUG96bmHFhCwgUG9sc2th!5e0!3m2!1spl!2snl!4v1712579399421!5m2!1spl!2snl"
+                    width="100%" height="250" class="responsive-map" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade">
+
+                </iframe>
             </div>
-            <div class="column">
-                <h4>Logowanie</h4>
-                <ul id="tekst">
-                    <li><a href="#">Zaloguj się</a></li>
-                    <li><a href="#">Zarejestruj się</a></li>
-                </ul>
-            </div>
-            <div class="column">
-                <h4>Nawigacja</h4>
-                <ul id="tekst">
-                    <li><a href="#">Strona główna</a></li>
-                    <li><a href="#">Kalendarz</a></li>
-                    <li><a href="#">Wydarzenia</a></li>
-                    <li><a href="#">Kontakt</a></li>
-                </ul>
+
+        <div id="background">
+            <div class="footer-backcolor">
+                <div class="footer-columns">
+                    <div class="column">
+                        <h4>Social Media</h4>
+                        <ul id="tekst">
+                            <li><a href="#">Odwiedź naszego facebooka</a></li>
+                            <li><a href="#">Zapraszamy na nasz profil na instagramie</a></li>
+                            <li><a href="#">Wejdź na naszego youtuba</a></li>
+                        </ul>
+                    </div>
+                    <div class="column">
+                        <h4>Logowanie</h4>
+                        <ul id="tekst">
+                            <li><a href="#">Zaloguj się</a></li>
+                            <li><a href="#">Zarejestruj się</a></li>
+                        </ul>
+                    </div>
+                    <div class="column">
+                        <h4>Nawigacja</h4>
+                        <ul id="tekst">
+                            <li><a href="#">Strona główna</a></li>
+                            <li><a href="#">Kalendarz</a></li>
+                            <li><a href="#">Wydarzenia</a></li>
+                            <li><a href="#">Kontakt</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
+</body>
+</html>

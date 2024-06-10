3fd4923cf26c637f2d43a8cf5a9cf7f7019c60f8
karolinastasik1205200 <karolinastasik1205200@gmail.com>
2024-06-10 17:06:18 +0200

    logowanie dla administratora

--- login.php
+++ login.php
@@ -7,6 +7,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = htmlspecialchars($_POST['email']);
     $password = $_POST['password'];
 
+    if ($email === 'a.eventarea@interia.pl' && $password === 'Admin1234!') {
+        $_SESSION['user_id'] = 'admin'; 
+        header("Location: administrator/admin.html");
+        exit();
+    }
+
     $sql = "SELECT id, password FROM users WHERE email = :email";
     $stmt = $db->prepare($sql);
     $stmt->bindParam(':email', $email, PDO::PARAM_STR);
@@ -16,9 +22,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
-        echo "Logowanie zakończone sukcesem!";
         header("Location: user_page.php");
         exit();
     } else {
         echo "Nieprawidłowa nazwa użytkownika lub hasło.";
     }
+}
